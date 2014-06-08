ca6bee73b733317992bcdbac5d2db18c9fee4ed5
Dallin Osmun <dosmun@gmail.com>
2014-06-08 19:56:37 -0400

    updated starter file

--- starter.sh
+++ starter.sh
@@ -3,5 +3,6 @@
 if [ $(ps -e -o uid,cmd | grep $UID | grep node | grep -v grep | wc -l | tr -s "\n") -eq 0 ]
 then
         export PATH=/usr/local/bin:$PATH
-        forever start --sourceDir /opt/nodejs/lanaosmunmusic.com server/app.js >> /var/log/nodejs/lanaosmunmusic.com.log 2>&1
+        cd /opt/nodejs/lanaosmunmusic.com
+        forever start server/app.js
 fi
